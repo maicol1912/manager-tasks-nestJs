b5fddcd2be5b826395694692c72262451c2c0421
maicol1912 <maicolarcila34@gmail.com>
2023-04-09 11:39:21 -0500

    configuracion de docker compose

--- src/database/init.sql
+++ src/database/init.sql
@@ -1,3 +1,4 @@
 -- CREATE DATABASE IF NOT EXISTS codrrdb
-SELECT 'CREATE DATABASE codrrdb'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'codrrdb')\gexec
+--SELECT 'CREATE DATABASE ecommerce_nest_prod'
+--WHERE NOT EXISTS (SELECT FROM ecommerce_nest_prod WHERE datname = 'ecommerce_nest_prod')\gexec
+SELECT 'CREATE DATABASE ecommerce_nest_prod' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ecommerce_nest_prod');
